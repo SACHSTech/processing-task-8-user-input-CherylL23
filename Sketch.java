f654b3e4d13b2aea5df041e34dcfb788b9684661
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-05-02 22:01:02 -0400

    slime house

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,96 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+
+  // used to enable typing letters
+  String message ="";
+
   public void settings() {
-	// put your size call here
+
     size(400, 400);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+
+    background(51, 139, 255);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  //setting up the drawHouse function
+  void drawHouse(float mouseX, float mouseY, float houseWidth, float houseHeight) {
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //biggest rectangle of the house
+    stroke(0);
+    fill(240, 235, 182);
+    rect(mouseX, mouseY, houseWidth, houseHeight);
+    
+    //roof positions
+    fill(207, 76, 33);
+    triangle((float) (mouseX), (float) (mouseY), (float) (mouseX + 100), (float) (mouseY - 100), (float) (mouseX + houseWidth) , (float) (mouseY));
+           
+    //window positions
+    fill(145, 229, 242);
+    rect((float) (mouseX + 10), (float) (mouseY + 30), (float) (houseWidth - 130), (float) (houseHeight - 90));
+     
+    //rectangle door positions
+    fill(82, 79, 53);
+    rect((float) (mouseX + 90), (float) (mouseY +30), (float) (houseWidth - 120), (float) (houseHeight - 30));
+         
+    // doorknob positions
+    fill(240, 252, 0);
+    ellipse((float) (mouseX + 155), (float) (mouseY + 100), (float) (houseWidth -185), (float) (houseHeight - 135));
+  }
+
+  public void draw() {
+
+    //draws out grass by pressing the mouse
+    if (mousePressed) {
+      stroke(0);
+      fill (0, 255, 106);
+      rect(mouseX, mouseY, 200, 120);
+    }
+ 
+    if (keyPressed) {
+      if (keyCode == CONTROL){
+        //summoning a slime
+        PImage imgSlime;
+        imgSlime = loadImage("RedSlime.png");
+        imgSlime.resize(100, 100);
+        image(imgSlime, mouseX, mouseY);
+      }
+      
+      //drawing different colored retangle clouds with different keys
+      if (key == 's') {
+        fill(255, 255, 255);
+        rect(mouseX, mouseY, 100, 20);
+      }
+
+      else if (key == 'i') {
+        fill(163, 152, 152);
+        rect(mouseX, mouseY, 100,20);
+      }
+
+      else if (key == 'e') {
+        fill(20, 20, 20);
+        rect(mouseX, mouseY, 100, 20);
+      }
+
+    }
+
+    //types out a message
+    fill(244, 250, 62);
+    text(message, 20, 150);
   }
   
-  // define other methods down here.
+  public void keyTyped() {
+
+    //displays the letters typed
+    message += key;
+  }
+
+  public void mouseClicked() {
+
+    //when clicked a house will be drawn
+    drawHouse(mouseX, mouseY, 200, 150);
+  }
 }
